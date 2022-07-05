9a61ed6dde6fb6fb58b615c0c098f7e20a8db252
Talita Saez <tltsaez@gmail.com>
2022-07-05 16:58:43 -0300

    resolução desafio 07

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const terceiraCor = document.getElementsByClassName('color')[2];
 terceiraCor.style.backgroundColor = 'red';
 const quartaCor = document.getElementsByClassName('color')[3];
 quartaCor.style.backgroundColor = 'green';
+
+//let colorblack = document.querySelector('div');
+//colorblack.classList.add ('selected');
+
+function selecionarCor() {
+  let classSelected = document.getElementsByClassName('color');
+  for (let index = 0; index < classSelected.length; index += 1) {
+    classSelected[index].addEventListener('click', elementClick);
+  }
+}
+
+function elementClick(event) {
+  let classSelected = document.getElementsByClassName('color');
+  console.log(classSelected);
+  for (let index = 0; index < classSelected.length; index += 1) {
+    classSelected[index].classList.remove('selected');
+  }
+
+  event.target.classList.add('selected');
+}
+
+selecionarCor();
