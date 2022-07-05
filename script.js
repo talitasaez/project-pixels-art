ab52eb7f7cd3b3eda60da243c3b922ab2b6eb14d
Talita Saez <tltsaez@gmail.com>
2022-07-04 21:08:45 -0300

    resolucao desafio 2, 3

---
+++ script.js
@@ -0,0 +1,8 @@
+const primeiraCor = document.getElementsByClassName('color')[0];
+primeiraCor.style.backgroundColor = 'black';
+const segundaCor = document.getElementsByClassName('color')[1];
+segundaCor.style.backgroundColor = 'blue';
+const terceiraCor = document.getElementsByClassName('color')[2];
+terceiraCor.style.backgroundColor = 'red';
+const quartaCor = document.getElementsByClassName('color')[3];
+quartaCor.style.backgroundColor = 'green';
